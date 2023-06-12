0d9a0aa06855f9fd1824877099f1d3d1a9735040
Jerone <jayvolney77@gmail.com>
2023-06-11 20:14:51 -0400

    Added Theme Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 "use client";
-
+import { useEffect } from "react";
 import styles from "./page.module.css";
 import { useState } from "react";
 
 export default function Home() {
-  const [theme, currentTheme] = useState("");
+  const themes = [
+    "linear-gradient(243.18deg, #5CEBD1 0%, #1A2B2C 100%)",
+    "linear-gradient(243.18deg, #0C05E7 0%, #9B2FCD 100%)",
+    "linear-gradient(243.18deg, #BCC92D 0%, #48C1C9 100%)",
+    "linear-gradient(243.18deg, #9065BC 0%, rgba(255, 0, 108, 0.38) 100%)",
+    "linear-gradient(243.18deg, #1C565E 0%, #3DC7DA 100%)",
+    "linear-gradient(243.18deg, #00C236 0%, #40B845 100%)",
+  ];
+  const [themeIndex, setThemeIndex] = useState(0);
+
+  useEffect(() => {
+    document.body.style.background = themes[themeIndex];
+  }, [themeIndex]);
+
+  const changeTheme = () => {
+    setThemeIndex((prevIndex) => (prevIndex + 1) % themes.length);
+  };
   return (
     <main className={styles.parent}>
       <section className={styles.containerLeft}>
         <section className={styles.containerLeftInner}>
           <div className={styles.header}>
             <img src="/cloud.png" className={styles.mainWeather} />
-            <button>Change Theme</button>
+            <button onClick={changeTheme}>Change Theme</button>
             <div className={styles.tempToggle}>
               <p>C</p>
               <p>F</p>
